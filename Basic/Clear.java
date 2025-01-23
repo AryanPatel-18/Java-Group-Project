456248737476ff5d1b74102394faa404eb283303
AryanPatel-18 <aryanpatel2593@gmail.com>
2025-01-23 16:05:53 +0530

    Added java file to clean database

---
+++ Basic/Clear.java
@@ -0,0 +1,16 @@
+package Basic;
+
+public class Clear {
+    public void clearDataBase(String id){
+        // For clearing the entire database
+    }
+
+    public void deleteUser(String id){
+        // For deleting users
+        // if id is of admin we can run the admin delete function
+    }
+
+    private void deleteAdmin(){
+        // For delete admin user
+    }
+}
